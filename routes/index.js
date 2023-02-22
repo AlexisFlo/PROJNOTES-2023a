fbef1f6f18723d6b0ba8d098920f0bd33939c136
Flowers <floresfernandezalexis05@gmail.com>
2023-02-22 07:29:59 -0600

    🐱‍👤 Random icon

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let iconSet = ["⭐","🖖","🐱‍👤"]
-  let icon = iconSet=[Math.floor(Math.random() * iconSet.length)];
+  let icon = iconSet=[Math.floor(Math.random() * 3)];
   res.render('index', { title: 'DWPCII-2023A', icon });
 });
 
