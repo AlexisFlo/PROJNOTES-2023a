bad433299f608423486996418e0ac513c1aa90d4
Flowers <floresfernandezalexis05@gmail.com>
2023-03-17 09:02:21 -0600

    ⭐ Babel in the front-end

--- client/index.js
+++ client/index.js
@@ -1,1 +1,27 @@
 console.log("🎉 Client Server working powered by webpack")
+
+// ES6 CODE
+// Default Parameters
+let show = (msg="No message given") => {
+    console.log(msg)
+}
+
+show();
+
+// ES6 CODE
+// Async Await
+function resolveAfter2Seconds() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('reslved');
+        }, 2000)
+    });
+}
+
+async function asyncCall() {
+    console.log("Calling");
+    const result = await resolveAfter2Seconds();
+    console.log(result); 
+}
+
+asyncCall();
