3ac1ff02b933e9e712f635ca47ac278c5027a599
Flowers <floresfernandezalexis05@gmail.com>
2023-03-29 08:57:40 -0600

    ⭐ ESlint Added

--- client/index.js
+++ client/index.js
@@ -1,29 +1,3 @@
 // Styles loading
 import './styles/style.css'
 console.log("🎉 Client Server working powered by webpack")
-
-// ES6 CODE
-// Default Parameters
-let show = (msg="No message given") => {
-    console.log(msg)
-}
-
-show();
-
-// ES6 CODE
-// Async Await
-function resolveAfter2Seconds() {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve('reslved');
-        }, 2000)
-    });
-}
-
-async function asyncCall() {
-    console.log("Calling");
-    const result = await resolveAfter2Seconds();
-    console.log(result); 
-}
-
-asyncCall();
