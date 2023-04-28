dfa144a1ec70b27d3c416c65b1a7fa21ab874d52
Flowers <floresfernandezalexis05@gmail.com>
2023-04-28 08:16:54 -0600

    🌟 Materializecss

--- client/index.js
+++ client/index.js
@@ -1,4 +1,14 @@
 // Styles loading
+/* eslint-disable no-console */
 import './styles/style.css';
+/* eslint-disable */
+// Importing Materialize CSS styles
+import 'materialize-css/dist/css/materialize.css';
+// Importing Materialize scripts
+import 'materialize-css/dist/js/materialize';
+/* eslint-enable */
 
-console.log('🎉 Styles loaded successfully 🎉');
+// Initializing Materialize scripts for interactivity
+M.AutoInit();
+
+console.log('Webpack Working Middleware!!! 📦');
